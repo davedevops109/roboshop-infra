4946cd6acf513fa2c03b423919d9d48bc8c6283f
EPAPRA <dave.devops109@gmail.com>
2023-08-09 13:51:32 +0530

    multibranch commit

--- main.tf
+++ main.tf
@@ -1,9 +1,23 @@
-module "network" {
-  source = "github.com/davedevops109/tf-module-vpc"
-  env = var.env
-  default_vpc_id = var.default_vpc_id
+module "vpc" {
+  source          = "github.com/davedevops109/tf-module-vpc"
+  env             = var.env
+  default_vpc_id  = var.default_vpc_id
 
-  for_each = var.vpc
-  cidr_block = each.value.cidr_block
+  for_each    = var.vpc
+  cidr_block  = each.value.cidr_block
 }
+
+module "subnets" {
+  source          = "github.com/davedevops109/tf-module-subnets"
+  env             = var.env
+  default_vpc_id  = var.default_vpc_id
+
+  vpc_id = module.vpc.vpc_id
+
+  for_each    = var.subnets
+  cidr_block  = each.value.cidr_block
+  availability_zone = each.value.availability_zone
+  name = each.value.name
+}
+
 
