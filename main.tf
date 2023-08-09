ca13f8b934395357100779305e991887cb4675dd
EPAPRA <dave.devops109@gmail.com>
2023-08-09 15:35:23 +0530

    multibranch commit

--- main.tf
+++ main.tf
@@ -7,18 +7,18 @@ module "vpc" {
   cidr_block  = each.value.cidr_block
 }
 
-#module "subnets" {
-#  source          = "github.com/davedevops109/tf-module-subnets"
-#  env             = var.env
-#  default_vpc_id  = var.default_vpc_id
-#
-#
-#  for_each    = var.subnets
-#  cidr_block  = each.value.cidr_block
-#  availability_zone = each.value.availability_zone
-#  name = each.value.name
-#  vpc_id = lookup(module.vpc, each.value.vpc_name, null)
-#}
+module "subnets" {
+  source          = "github.com/davedevops109/tf-module-subnets"
+  env             = var.env
+  default_vpc_id  = var.default_vpc_id
+
+
+  for_each    = var.subnets
+  cidr_block  = each.value.cidr_block
+  availability_zone = each.value.availability_zone
+  name = each.value.name
+  vpc_id = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
+}
 
 output "vpc_id" {
   value = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
