f03466cadaa123d4bc259f82589fd7e007faabb1
EPAPRA <dave.devops109@gmail.com>
2023-08-10 11:59:46 +0530

    multibranch commit

--- main.tf
+++ main.tf
@@ -13,12 +13,14 @@ module "subnets" {
   default_vpc_id  = var.default_vpc_id
 
 
-  for_each    = var.subnets
-  cidr_block  = each.value.cidr_block
+  for_each          = var.subnets
+  cidr_block        = each.value.cidr_block
   availability_zone = each.value.availability_zone
-  name = each.value.name
-  vpc_id = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
+  name              = each.value.name
+  vpc_id            = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
   vpc_peering_connection_id = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_peering_connection_id", null)
+  internet_gw       = lookup(each.value, "internet_gw", false)
+  nat_gw            = lookup(each.value, "nat_gw", false)
 }
 
 output "vpc_id" {
