31b860b1ab68516031a2edea89aed85c4fdb7078
EPAPRA <dave.devops109@gmail.com>
2023-08-14 12:22:56 +0530

    multibranch commit

--- main.tf
+++ main.tf
@@ -10,27 +10,9 @@ module "vpc" {
   availability_zone = each.value.availability_zone
 }
 
-#module "subnets" {
-#  source          = "github.com/davedevops109/tf-module-subnets"
-#  env             = var.env
-#  default_vpc_id  = var.default_vpc_id
-#
-#
-#  for_each          = var.subnets
-#  cidr_block        = each.value.cidr_block
-#  availability_zone = each.value.availability_zone
-#  name              = each.value.name
-#  vpc_id            = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
-#  vpc_peering_connection_id = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_peering_connection_id", null)
-#  internet_gw       = lookup(each.value, "internet_gw", false)
-#  nat_gw            = lookup(each.value, "nat_gw", false)
-#}
-
-#output "subnet_ids" {
-#  value = module.subnets
-#}
-
-output "out" {
-  value = module.vpc
+module "docdb" {
+  source = "github.com/davedevops109/tf-module-docdb"
+  env    = var.env
+  subnet_ids  =
 }
 
