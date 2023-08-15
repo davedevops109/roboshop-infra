e2f2ef7a203107bc1b1c5e4e35f0a419ab076a0b
EPAPRA <dave.devops109@gmail.com>
2023-08-15 09:22:24 +0530

    multibranch commit

--- main.tf
+++ main.tf
@@ -10,14 +10,6 @@ module "vpc" {
   availability_zone = each.value.availability_zone
 }
 
-module "docdb" {
-  source = "github.com/davedevops109/tf-module-docdb"
-  env    = var.env
-
-  for_each = var.docdb
-  subnet_ids  = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), "private_subnet_ids", null), each.value.subnets_name, null), "subnet_ids", null)
-}
-
 output "vpc" {
   value = module.vpc
 }
