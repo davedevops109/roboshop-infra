c1b3d14bbe8e8d2e4c9748ffeb25eeb5b171d56e
EPAPRA <dave.devops109@gmail.com>
2023-08-21 14:35:16 +0530

    multibranch commit

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ module "vpc" {
 }
 
 module "docdb" {
-  source = "https://github.com/davedevops109/tf-module-docdb.git"
+  source = "github.com/davedevops109/tf-module-docdb.git"
   env    = var.env
 
   for_each    = var.docdb
