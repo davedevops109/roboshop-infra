431a694fbf189d15394f649cda5f2041c2a92c67
EPAPRA <dave.devops109@gmail.com>
2023-08-22 12:47:34 +0530

    multibranch commit

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ module "vpc" {
   env             = var.env
   default_vpc_id  = var.default_vpc_id
 
-  for_each    = var.vpc
-  cidr_block  = each.value.cidr_block
-  public_subnets     = each.value.public_subnets
+  for_each            = var.vpc
+  cidr_block          = each.value.cidr_block
+  public_subnets      = each.value.public_subnets
   private_subnets     = each.value.private_subnets
-  availability_zone = each.value.availability_zone
+  availability_zone   = each.value.availability_zone
 }
 
 module "docdb" {
@@ -16,6 +16,9 @@ module "docdb" {
 
   for_each    = var.docdb
   subnet_ids  = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), "private_subnet_ids", null), each.value.subnets_name, null), "subnet_ids", null)
+  vpc_id = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
+  allow_cidr = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), "private_subnet_ids", null), "app", null), "subnet_ids", null)
+
 }
 
 output "vpc" {
